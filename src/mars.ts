5ba23827bf3a022a652285bc620a88cae5d8470d
Benson <vivionanoha@gmail.com>
2023-02-11 14:13:08 +0000

    update mars with point value

--- src/mars.ts
+++ src/mars.ts
@@ -1,25 +1,46 @@
+import { Command } from './globalFunction/command';
 import { clear, print, askQuestion } from './console';
 
+
+ interface Point {
+    x: number
+    y: number
+ 
+   }
 export function restartApp(): void {
     clear(false);
     print('--------------------------');
     print('| Welcome to Mars! |');
     print('--------------------------');
 
-    askQuestion(`Where do you want to go ?  `, startingPoint);
+    askQuestion(`Where do you want to start ?  `, startPoint);
 }
 
-function startingPoint(pointLocation: string): void {
+function startPoint(pointLocation: string): void {
 
     if (pointLocation) {
-        const point = [...pointLocation];
-        const direction =  'N' ; 
-        print(`The Start Location is ' , ${point} , ${direction} ,!`) ; 
-        // return xxxx(point , direction) ;
+       const firstPoint :Point = { x : parseInt([...pointLocation][0]) , y : parseInt([...pointLocation][0])}
+        const direction ='N'
+        print(`The Start Location is ${firstPoint.x},${firstPoint.y} , ${direction} !`) ; 
+        return  askQuestion(`Where do you want to go ? ` , insertDirection)
     } else {
     return backToStartPoint();
     }
+}
 
+function insertDirection(pointDirection : string){
+
+    // if(pointDirection){
+    //     const directionCommend = [...pointDirection];
+
+    //     directionCommend.forEach((x) => {
+    //         Command(x);
+
+    //         print('actual Location');
+    //         return ;
+    //     })
+
+    // }
 }
 
 export function backToStartPoint(): void {
